0f9be210f6249e5a842a98c6233060fc01a62b34
EchoTerror <najikore@mail.ru>
2024-01-31 16:24:40 +0300

    Delete pring functuons

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int a = 1, b = 2;
-        System.out.println(a + "+" + b + " = " + (a+b));
-        System.out.println("FeatureY");
     }
 }
